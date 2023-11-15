4db56111b1c3e5085fea393d8f7d37d1056e5f57
Samuel Antenor <asamuelandre@gmail.com>
2023-11-15 10:12:07 -0500

    modification du plugins pour l'affichage de la section

--- js/profs.js
+++ js/profs.js
@@ -5,7 +5,7 @@
     let currentIndex = 0;
     let articlesData = [];
     let featuredImage;
-  
+
     fetch(url)
         .then(function (response) {
             if (!response.ok) {
@@ -24,7 +24,7 @@
         .catch(function (error) {
             console.error("Erreur lors de la récupération des données :", error);
         });
-  
+
     function afficherEnseignants() {
         let urlEnseignants = "http://localhost:8080/5w5/wp-json/wp/v2/posts?categories=2&_embed";
         fetch(urlEnseignants)
@@ -35,51 +35,75 @@
             })
             .catch(error => console.log(error));
     }
-  
-    function resetAnimation(element) {
-        element.style.animation = 'none';
-        setTimeout(() => {
-            element.style.animation = '';
-        }, 0);
+
+    function toggleArticleContent(index) {
+        const allArticleContents = document.querySelectorAll('.article-content');
+        const targetArticleContent = document.querySelector(`.article-content[data-index="${index}"]`);
+        allArticleContents.forEach(content => {
+            content.style.display = 'none'; // Hide all article contents
+            content.parentElement.classList.remove('expanded'); // Remove expanded class from parent
+        });
+        targetArticleContent.style.display = 'block'; // Show the target article content
+        targetArticleContent.parentElement.classList.add('expanded'); // Add expanded class to parent
     }
-  
+    
     function updateArticleDisplay() {
-        divImages.innerHTML = "";
-        const article = articlesData[currentIndex];
-        const uneImage = document.createElement("div");
-        uneImage.className = "article";
-        uneImage.innerHTML = `
-            <h4>${article.title.rendered}</h4>
-            <p>${article.content.rendered}</p>
+        divImages.innerHTML = ""; // Clear the container first.
+    
+        // Show only 4 articles at a time
+        for (let i = 0; i < 4; i++) {
+            let displayIndex = (currentIndex + i) % articlesData.length;
+            const article = articlesData[displayIndex];
+            let featuredImgSrc = getFeaturedImageSrc(article);
+    
+            let articleElement = createArticleElement(article, featuredImgSrc, displayIndex);
+            divImages.appendChild(articleElement);
+        }
+    }
+    
+
+    function createArticleElement(article, imgSrc, index) {
+        const articleElement = document.createElement("div");
+        articleElement.className = "article";
+        articleElement.innerHTML = `
+            <div class="article-content" data-index="${index}" style="display: none;">
+                <h4>${article.title.rendered}</h4>
+                <p>${article.content.rendered}</p>
+            </div>
+            <img src="${imgSrc}" alt="${article.title.rendered}" class="article-image" data-index="${index}" />
         `;
+
+        const imgElement = articleElement.querySelector('.article-image');
+        imgElement.addEventListener('click', function () {
+            toggleArticleContent(index);
+        });
+
+        return articleElement;
+    }
+
+    function getFeaturedImageSrc(article) {
+        let featuredImgSrc = "";
         if (article._embedded && article._embedded["wp:featuredmedia"]) {
             featuredImage = article._embedded["wp:featuredmedia"][0];
             if (featuredImage.source_url) {
-                uneImage.innerHTML += `<img src="${featuredImage.source_url}" alt="${article.title.rendered}" />`;
+                featuredImgSrc = featuredImage.source_url;
             }
         }
-        divImages.appendChild(uneImage);
-        resetAnimation(uneImage);
-  
-        const allImages = divImages.querySelectorAll('img');
-        allImages.forEach(img => img.classList.remove('active'));
-        const activeImage = divImages.querySelector(`img[src="${featuredImage.source_url}"]`);
-        if (activeImage) {
-            activeImage.classList.add('active');
-        }
+        return featuredImgSrc;
     }
-  
+
+    // Navigation buttons
     const leftButton = document.querySelector('.btn-prev');
     const rightButton = document.querySelector('.btn-next');
-  
+
     leftButton.addEventListener('click', function () {
-        currentIndex = (currentIndex - 1 + articlesData.length) % articlesData.length;
-        updateArticleDisplay();
+        currentIndex = (currentIndex - 4 + articlesData.length) % articlesData.length;
+    updateArticleDisplay();
     });
-  
+
     rightButton.addEventListener('click', function () {
-        currentIndex = (currentIndex + 1) % articlesData.length;
-        updateArticleDisplay();
+        currentIndex = (currentIndex + 4) % articlesData.length;
+    updateArticleDisplay();
     });
-  })();
-  
+
+})();
